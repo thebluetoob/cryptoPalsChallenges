8c0a5eaedef2d2523fb78c3f50bde1e5d8eaded3
Ben Ellett <github@benjaminellett.com>
2020-05-30 13:34:56 +1000

    add output to set 1/challenge 1

--- set_1/challenge_1-convert_hex_to_base64.py
+++ set_1/challenge_1-convert_hex_to_base64.py
@@ -22,3 +22,8 @@ encoded_bytes = base64.b64encode(raw_bytes)
 print('Input: ' + original_hex)
 print('Decoded message (from hex): ' + raw_bytes.decode())
 print('Encoded message (to base64): ' + encoded_bytes.decode())
+
+# Output:
+# Input: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+# Decoded message (from hex): I'm killing your brain like a poisonous mushroom
+# Encoded message (to base64): SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
